d5b674469ac43959702199ea727a0fba6a79246a
Va&Cob <ratthaninw@gmail.com>
2025-07-22 13:29:09 +0700

    squareline studio added

---
+++ sql/.printpoop_events.py
@@ -0,0 +1,28 @@
+
+def wait5second(event_struct):
+    return
+
+
+def openSetupPortal(event_struct):
+    return
+
+
+def closeSetupScreen(event_struct):
+    return
+
+
+def switch_previous_page(event_struct):
+    return
+
+
+def switch_next_page(event_struct):
+    return
+
+
+def switch_printpoop_page(event_struct):
+    return
+
+
+def switch_status_page(event_struct):
+    return
+
